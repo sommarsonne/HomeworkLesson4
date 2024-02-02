a006d2801bf09a0b839429f47692e5b9eb73060f
sommarsonne <mumudynasty@gmail.com>
2024-02-02 16:49:13 +0300

    5 к 1

---
+++ 5 to 1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _5_to_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 5; a > 0; a--)
+
+                Console.WriteLine($"{a}");
+        }
+    }
+}
+
