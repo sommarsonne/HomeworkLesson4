76899a701ed9acecd8158a4f7aeb417b4c48dc09
sommarsonne <mumudynasty@gmail.com>
2024-02-02 16:36:36 +0300

    1 to 99 for

---
+++ 1 to 99 for/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _1_to_99_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a < 100; a++)
+                if (a % 2 == 0)
+                {
+                   Console.WriteLine($"{a}");
+            
+                }
+
+        }
+    }
+}
+
