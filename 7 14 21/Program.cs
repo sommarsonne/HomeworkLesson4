17ac29b6032d89fcddd1f9f927a51ab20ae65db3
sommarsonne <mumudynasty@gmail.com>
2024-02-03 06:33:44 +0300

    7 14 21

---
+++ 7 14 21/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _7_14_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 0;
+
+            while (a < 98)
+            {
+                a += 7;
+                
+                Console.WriteLine($"{a}");
+
+            }
+        } 
+    }
+}
