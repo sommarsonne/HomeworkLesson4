b7cbdf46e6702ab10fba7836bc9410ad15799fad
sommarsonne <mumudynasty@gmail.com>
2024-02-03 05:27:58 +0300

    Mental block

---
+++ 1 to UserInput/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _5_to_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите число");
+            //string input = Console.ReadLine();
+
+            int i = 1;
+            int b = 30;
+            
+
+            while (true)
+            {
+                
+                int v = i + i++;
+                if (v < b)
+                    Console.WriteLine(v);
+
+                else
+                    break;
+
+                
+            }
+
+            
+         
+
+
+
+
+
+
+
+
+        }
+    }
+}
